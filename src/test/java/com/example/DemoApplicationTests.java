f1f29c074947bd0ae3fcff70cda9d74c769267df
Sofiane <Vous@exemple.com>
2019-05-16 10:31:10 +0200

    modify

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -13,4 +13,54 @@ public class DemoApplicationTests {
 	public void contextLoads() {
 	}
 
+
+    private static Logger logger = Logger.getLogger(AppTest.class);
+  
+  @Test
+    public void shouldAnswerWithTrue()
+    {
+	logger.info("Hello World!");        
+	assertTrue( true );
+        
+    }
+
+	public Integer addition(Integer a, Integer b)
+	    {
+		Integer somme;
+		somme = a + b;
+	        return somme;
+	   }
+
+	@Test
+	public void additionner_should_return()
+	{
+		Integer somme = addition(2, 3); 
+		if (somme == 5) {
+   		 assertTrue( true );
+		} else {
+		assertTrue( false );
+		}
+
+	}
+	
+	
+	public Integer soustraction(Integer a, Integer b)
+	    {
+		Integer somme;
+		somme = a - b;
+	        return somme;
+	   }
+	
+	@Test
+	public void soustraction_should_return()
+	{
+		Integer somme = soustraction(2, 3); 
+		if (somme < 5) {
+   		 assertTrue( true );
+		} else {
+		assertTrue( false );
+		}
+
+	}
+
 }
